0a126c202707d13ac1c0e596591cb08f2a4a10e7
Andrei Voicu <dreivoicu@gmail.com>
2020-07-13 19:53:01 +0300

    compiles

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -1,32 +1,59 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { ConnectionPositionPair, Overlay, OverlayRef } from '@angular/cdk/overlay';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnDestroy,
+  ViewContainerRef,
+  TemplateRef,
+  ViewChild
+} from '@angular/core';
+import {
+  ConnectionPositionPair,
+  Overlay,
+  OverlayRef,
+  FlexibleConnectedPositionStrategy
+} from '@angular/cdk/overlay';
 import { FormControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { debounceTime, filter, takeUntil } from 'rxjs/operators';
+import { TemplatePortal } from '@angular/cdk/portal';
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss']
 })
-export class SelectComponent implements OnInit {
+export class SelectComponent implements OnInit, OnDestroy {
   @Input() model: any;
   @Input() labelKey = 'label';
   @Input() idKey = 'id';
   @Input() options = [];
+  @Input() optionTpl: TemplateRef<any>;
 
   @Output() selectChange = new EventEmitter<void>();
   @Output() closed = new EventEmitter<void>();
 
+  @ViewChild('origin') origin: HTMLButtonElement;
+  @ViewChild('dropdown') dropdown: TemplateRef<any>;
+
+  visibleOptions = 4;
+
   searchControl = new FormControl();
 
-  private overlayRef: OverlayRef;
+  private unsubscribe$: Subject<void>;
+  private overlayRef: OverlayRef | null;
   private originalOptions = [];
 
-  constructor(
-    private overlay: Overlay
-  ) { }
+  constructor(private vcr: ViewContainerRef, private overlay: Overlay) {}
 
-  get isOpen() {
+  get isOpen(): boolean {
     return true;
+  }
+
+  get label(): any {
+    return this.model ? this.model[this.labelKey] : 'Select...';
   }
 
   ngOnInit(): void {
@@ -34,16 +61,42 @@ export class SelectComponent implements OnInit {
 
     if (this.model !== undefined) {
       this.model = this.options.find(
-        currentOption => currentOption[this.idKey] === this.model
+        (currentOption) => currentOption[this.idKey] === this.model
       );
     }
+
+    this.searchControl.valueChanges
+      .pipe(takeUntil(this.unsubscribe$), debounceTime(300))
+      .subscribe((term) => this.search(term));
   }
 
-  get label() {
-    return this.model ? this.model[this.labelKey] : 'Select...';
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
   }
 
-  isActive(option: any) {
+  open(): void {
+    if (this.overlay) {
+      this.close();
+    }
+
+    this.overlayRef = this.overlay.create({
+      width: this.origin.offsetWidth,
+      maxHeight: this.visibleOptions * 32,
+      backdropClass: '',
+      scrollStrategy: this.overlay.scrollStrategies.reposition(),
+      positionStrategy: this.getOverlayPosition()
+    });
+
+    const template = new TemplatePortal(this.dropdown, this.vcr);
+    this.overlayRef.attach(template);
+  }
+
+  private close(): void {
+    this.overlayRef?.detach();
+    this.overlayRef = null;
+  }
+
+  isActive(option: any): boolean {
     if (!this.model) {
       return false;
     }
@@ -52,8 +105,8 @@ export class SelectComponent implements OnInit {
   }
 
   search(value: string): void {
-    this.options = this.originalOptions.filter(
-      option => (option[this.labelKey] as any[]).includes(value)
+    this.options = this.originalOptions.filter((option) =>
+      (option[this.labelKey] as any[]).includes(value)
     );
   }
 
@@ -62,4 +115,23 @@ export class SelectComponent implements OnInit {
     this.selectChange.emit(option[this.idKey]);
   }
 
+  private getOverlayPosition(): FlexibleConnectedPositionStrategy {
+    const positions = [
+      new ConnectionPositionPair(
+        { originX: 'start', originY: 'bottom' },
+        { overlayX: 'start', overlayY: 'top' }
+      ),
+      new ConnectionPositionPair(
+        { originX: 'start', originY: 'top' },
+        { overlayX: 'start', overlayY: 'bottom' }
+      )
+    ];
+
+    return this.overlay
+      .position()
+      .flexibleConnectedTo(this.origin)
+      .withPositions(positions)
+      .withFlexibleDimensions(false)
+      .withPush(false);
+  }
 }
